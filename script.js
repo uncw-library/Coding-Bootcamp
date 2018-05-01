80a133373bcf5f496f1a104b0d6bbc129e80aa07
ElliotSimpson <31009469+ElliotSimpson@users.noreply.github.com>
2018-05-01 17:03:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
         $.ajax({
             type: 'POST',
             url: "https://libcat.uncw.edu/iii/sierra-api/swagger/index.html/v5/token",
-            headers: {'Access-Control-Allow-Origin': '*'},
+            headers: {'Access-Control-Allow-Origin': '*'},            
+            xhrFields: {
+                withCredentials: true
+            },
             beforeSend: function (xhr) {
                 xhr.setRequestHeader ("Authorization", "Basic M21pQ0Y2aW1VQk1OcE95WXE3ekwvZ2M4YUcrTjpmcmVlemVyYXk=");
             },
