1698b5f1666005896fe6b23fc58473cc427b2c02
ElliotSimpson <31009469+ElliotSimpson@users.noreply.github.com>
2018-05-02 08:45:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#itemForm").click(function(){
         $.ajax({
-            url: "https://libcat.uncw.edu:443/iii/sierra-api/v5/token",
+            url: "https://libcat.uncw.edu/iii/sierra-api/v5/token",
             headers: {'Access-Control-Allow-Origin': '*'},
             beforeSend: function (xhr) {
                 xhr.setRequestHeader ("Authorization", "Basic M21pQ0Y2aW1VQk1OcE95WXE3ekwvZ2M4YUcrTjpmcmVlemVyYXk=");
