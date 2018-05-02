d16bffc688620923a4579f8ea92807246bfdd0e9
ElliotSimpson <31009469+ElliotSimpson@users.noreply.github.com>
2018-05-02 08:13:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
             type: 'POST',
             url: "https://libcat.uncw.edu:443/iii/sierra-api/v5/token",
             beforeSend: function (xhr) {
-                xhr.setRequestHeader ("Authorization", "Basic M21pQ0Y2aW1VQk1OcE95WXE3ekwvZ2M4YUcrTjpmcmVlemVyYXk="));
+                xhr.setRequestHeader ("Authorization", "Basic M21pQ0Y2aW1VQk1OcE95WXE3ekwvZ2M4YUcrTjpmcmVlemVyYXk=");
             },
             success: function(resultData) { 
                 $('#titleSpan').html(resultData.title);
