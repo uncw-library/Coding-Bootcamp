5869c3137d76d4faa008337e7229929083baf8ce
ElliotSimpson <31009469+ElliotSimpson@users.noreply.github.com>
2018-05-03 10:26:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
     $("#itemForm").click(function(){
         var barcode = $('#barcode').val();
+        console.log(barcode);
         $.ajax({
             method: 'GET',
             url: "https://item-lookup.libapps.uncw.edu/" + barcode,
             success: function(resultData) { 
-                $('#titleSpan').html(resultData);
-                console.log(JSON.stringify(resultData)); 
+                console.log(resultData);
+                if (resultData.bib_record) {
+                    $('#titleSpan').html(resultData.bib_record.title);
+                    $('#authorSpan').html(resultData.bib_record.author);
+                    $('#callNumberSpan').html(resultData.item_record.callNumber);
+                    $('#statusSpan').html(resultData.item_record.status.display);
+                    $('#locationSpan').html(resultData.item_record.location.name);
+                }
+                //$('#statusSpan').html(resultData.status.display);
             },
             error: function(err){
                 console.log(JSON.stringify(err));
